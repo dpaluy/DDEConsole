39ce656ea643cf17d309ccd97ca8bac9c96f5201
dpaluy <dpaluy@c3da5284-7a2d-4169-a6cf-ff33e6757432>
2011-01-08 11:27:17 +0000

    git-svn-id: http://svn.paluy.org/myprojects/DDE/DDEConsole/trunk@341 c3da5284-7a2d-4169-a6cf-ff33e6757432

---
+++ DDEConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDEConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("DDE Console");
+
+        }
+    }
+}
