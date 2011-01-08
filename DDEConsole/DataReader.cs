384305f35e2cb821972fb85f08650955b214d6a3
dpaluy <dpaluy@c3da5284-7a2d-4169-a6cf-ff33e6757432>
2011-01-08 22:23:56 +0000

    Adding XML config WIP
    git-svn-id: http://svn.paluy.org/myprojects/DDE/DDEConsole/trunk@345 c3da5284-7a2d-4169-a6cf-ff33e6757432

---
+++ DDEConsole/DataReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+
+namespace DDEConsole
+{
+    public class DataReader
+    {
+        private XmlReader reader;
+        public DataReader(String fileName)
+        {
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            reader = new XmlTextReader(stream);
+        }
+
+        public void Read()
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.Value);
+            }
+        }
+    }
+}
