eed443b4ba76350aed9acc1eb768a1a12fade3e6
dpaluy <dpaluy@c3da5284-7a2d-4169-a6cf-ff33e6757432>
2011-01-24 12:23:51 +0000

    IV Graph WIP
    git-svn-id: http://svn.paluy.org/myprojects/DDE/DDEConsole/trunk@360 c3da5284-7a2d-4169-a6cf-ff33e6757432

---
+++ IV/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace IV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.zgCall1 = new ZedGraph.ZedGraphControl();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.statusStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(769, 316);
+            this.panel1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 294);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(769, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.zgCall1);
+            this.splitContainer1.Size = new System.Drawing.Size(769, 294);
+            this.splitContainer1.SplitterDistance = 383;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // zgCall1
+            // 
+            this.zgCall1.AutoScroll = true;
+            this.zgCall1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zgCall1.Location = new System.Drawing.Point(0, 0);
+            this.zgCall1.Name = "zgCall1";
+            this.zgCall1.ScrollGrace = 0;
+            this.zgCall1.ScrollMaxX = 0;
+            this.zgCall1.ScrollMaxY = 0;
+            this.zgCall1.ScrollMaxY2 = 0;
+            this.zgCall1.ScrollMinX = 0;
+            this.zgCall1.ScrollMinY = 0;
+            this.zgCall1.ScrollMinY2 = 0;
+            this.zgCall1.Size = new System.Drawing.Size(383, 294);
+            this.zgCall1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 316);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "IV Graphs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private ZedGraph.ZedGraphControl zgCall1;
+    }
+}
+
---
+++ IV/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace IV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateGraph(zgCall1);
+            SetSize();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetSize();
+        }
+
+        private void SetSize()
+        {
+            zgCall1.Location = new Point(10, 10);
+            // Leave a small margin around the outside of the control
+            zgCall1.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 20);
+        }
+
+        private void CreateGraph(ZedGraphControl zgc)
+        {
+            GraphPane myPane = zgc.GraphPane;
+
+            // Set the titles and axis labels
+            myPane.Title.Text = "Call IV";
+            myPane.XAxis.Title.Text = "Strike";
+            myPane.YAxis.Title.Text = "IV";
+
+            // Make up some data arrays based on the Sine function
+            double x, y;
+            PointPairList list = new PointPairList();
+            for (int i = 0; i < 37; i++)
+            {
+                x = ((double)i - 18.0) / 5.0;
+                y = x * x + 1.0;
+                list.Add(x, y);
+            }
+
+            // Generate a red curve with diamond
+            // symbols, and "Porsche" in the legend
+            LineItem myCurve = myPane.AddCurve("CALL",
+               list, Color.Green, SymbolType.Diamond);
+
+            // Set the Y axis intersect the X axis at an X value of 0.0
+            myPane.YAxis.Cross = 0.0;
+            // Turn off the axis frame and all the opposite side tics
+            myPane.Chart.Border.IsVisible = false;
+            myPane.XAxis.MajorTic.IsOpposite = false;
+            myPane.XAxis.MinorTic.IsOpposite = false;
+            myPane.YAxis.MajorTic.IsOpposite = false;
+            myPane.YAxis.MinorTic.IsOpposite = false;
+
+            // Calculate the Axis Scale Ranges
+            zgc.AxisChange();
+        }
+    }
+}
